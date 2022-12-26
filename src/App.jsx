e259602d6a1537b6669e9acb4cfb699e014d3d7f
aokimikats <80706196+aokimikats@users.noreply.github.com>
2022-12-26 18:34:03 +0100

    fix: added post-messages and added deployment scripts

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import './css/style.css';
 import AOS from 'aos';
 
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
 
 function App() {
@@ -38,8 +38,8 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        {/* <Route path="/signin" element={<SignIn />} /> */}
+        {/* <Route path="/signup" element={<SignUp />} /> */}
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </>
